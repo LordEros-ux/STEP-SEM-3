ef983f917c20b619b79607a173393037292929d0
LordEros-ux <aryansingh.250807@gmail.com>
2026-08-08 09:43:34 +0530

    Initial commit

---
+++ FileExtensionValidator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FileExtensionValidator {
+
+    static String validateFileExtension(String filename) {
+        int lastDot = filename.lastIndexOf('.');
+
+        if (lastDot == -1) {
+            return "Rejected — invalid file type";
+        }
+
+        String ext = filename.substring(lastDot + 1);
+
+        if (ext.equalsIgnoreCase("pdf") ||
+            ext.equalsIgnoreCase("docx") ||
+            ext.equalsIgnoreCase("zip")) {
+            return "Accepted";
+        }
+
+        return "Rejected — invalid file type";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String filename = sc.nextLine();
+
+        System.out.println(validateFileExtension(filename));
+    }
+}
